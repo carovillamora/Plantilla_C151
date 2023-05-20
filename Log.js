ab32af22121d7abee38344e66847bf602ff38e80
carovillamora <94028506+carovillamora@users.noreply.github.com>
2023-05-20 13:44:02 -0600

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,19 @@
+// Registrando el componente en log-component.js
+AFRAME.registerComponent('log', {
+    schema: {
+      //Datos
+    },
+    init: function () {
+      //Hacer algo cuando el componente se adjunte por primera vez
+    },
+    update: function(){
+      //Hacer algo cuando los datos del componente se actualicen
+    },
+    remove: function(){
+      //Hacer algo cuando el componente o su entidad se separen
+    },
+    tick: function(){
+      //Hacer algo en cada actualización de escena o fotograma
+    }
+  });
+
